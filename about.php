d13a6ccb5874904565260cdb6c53647389d728ae
yashwanthkothamasu <87110886+yashwanthkothamasu@users.noreply.github.com>
2021-12-06 13:56:03 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Online Hotel.Com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="css/style.css"rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body style="margin-top:50px;">
+<?php
+      include('Menu Bar.php')
+  ?><br>
+<div class="container-fluid text-center">
+<div class="container"> 
+  <div class="row content">
+    <div class="col-sm-12">
+      <h1 id="font">Hotel.com</h1><br>
+      <p class="text-justify">Hotel Management System allows users to handle hotel activities online. Customers can be provided with different services like checking in, checking out, payments, search for required rooms, check availability etc. Customers can cancel bookings online easily. User can check available rooms online.
+       Existing system is problematic for both customer and manager. It is difficult for customers if rooms are unavailable after reaching location. Here, hotel management system helps customer book hotel room in advance so, they don’t face issue regarding shortage of room. It also helps manager with overview of hotel.
+       Hotel management system makes easy for users to check availability and final bill while checking out and make payments. It is also easy for manager to generate reports of customers, employees, organize and store data of hotel. Separate module for employee information can be made and enquiry about any customer or employee can be made either by name or id.
+        Hotel management system is more efficient and user-friendly than existing system of entering data manually and storing. It reduces man power and can help to overcome problems caused by human errors. It is easy to handle for both manager and customer.</p>
+<h1 align="center"id="font">Services</h1><br>
+<p class="text-justify">System displays availability and rate for all rooms.
+System should allow customer to confirm or cancel the booking.
+System should record and update booking details into database.
+System allows customer to pay bill via any online mode (credit or debit card, UPI) 
+System should allow Receptionist to update, add or delete booking information.
+System enables receptionist to access to customer desk portal for providing response to customer inquiry.
+System should generate financial and customer report for manager.
+System should enable manager full modification access to customer, booking and room information.
+</p>
+    </div>
+  </div><br><br>
+  <div class="row"align="center">
+  <!--map Start Here-->
+  <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+      <a href="#" type="button" data-toggle="modal" data-target="#myModal"><img src="image/icon/icon-01.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+  </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <iframe class="frame"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9012415990155!2d77.37085911440647!3d28.602739392180986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce57ca5ff4435%3A0x6de631b7a5bfdacb!2sUnion+Bank+Of+India!5e0!3m2!1sen!2sin!4v1555502551059!5m2!1sen!2sin" width="550"height="300" frameborder="0" style="border:1" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>      
+<!--Map Close Here-->
+        <div class="col-sm-2">
+          <h4><b>Address</b></h4>
+          <b>SRM University, Andhrapradesh</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-02.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Phone</b></h4>
+              <b>(+91) 18110010097</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-03.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Email-Id</b></h4>
+              <b>SRM@gmail.com</b>
+        </div>
+  </div><br><br>
+</div>
+</div>
+<?php
+  include('Footer.php')
+?>
+</body>
+</html>
